94ee1cd3f88af9604afc046286ed017e7476c49b
Andrei <Andrei@mail.ru>
2022-12-14 21:20:29 +0300

    написан метод нахождения количества строк в массиве <= 3

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿int AmountElements(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Count(); i++)
+    {
+        if (array[i].Length <= 3) count += 1;
+    }
+    return count;
+}
